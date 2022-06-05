6e87cbc7e5bdf94512f48913c699b34fa7ddd74d
Classyham <theleagueoflegendsnoob@gmail.com>
2022-06-05 16:31:47 +0100

    Add project files.

---
+++ TimeshiftersMod/Components/RenderingTweaks.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Rendering;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+namespace TimeshiftersMod.Components
+{
+    class RenderingTweaks : MonoBehaviour
+    {
+        void Awake()
+        {
+            SceneManager.sceneLoaded += OnLevelLoaded;
+            
+        }
+
+        void OnLevelLoaded(Scene scene, LoadSceneMode mode)
+        {
+            QualitySettings.vSyncCount = 1;
+        }
+
+        /*
+        void OnGUI()
+        {
+            GUI.Label(new Rect(10,10,100,40),"Vsync On");
+        }
+        */
+    }
+}
---
+++ TimeshiftersMod/Entry.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TimeshiftersMod.Components;
+using UnityEngine;
+
+namespace TimeshiftersMod
+{
+    public class Entry
+    {
+        public static GameObject o;
+
+        public static void Main()
+        {
+            //initialize Gameobject
+            Entry.o = new GameObject();
+            Entry.o.AddComponent<RenderingTweaks>();
+
+            //Make sure the object we create doesn't get destroyed between scene transitions. 
+            UnityEngine.Object.DontDestroyOnLoad(Entry.o);
+        }
+    }
+}
